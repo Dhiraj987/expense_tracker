96d8bcfa671164ed4ee1250564eaf196a48078b6
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-12-01 01:11:57 -0500

    initial routes for income and expense

---
+++ server/index.js
@@ -0,0 +1,40 @@
+/** this file will define the app and set up some dependencies
+ * it will use
+ * this will also set up the environment and connect api to
+ * the app
+ * */
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+// create an object from the express class
+const app = express();
+
+// used for cross validation while in dev mode
+app.use(cors());
+
+// connect the expenses routes to the app
+import expenseRoute from "./routes/expense.js";
+app.use("/exp", expenseRoute);
+
+// connect the income routes to the app
+import incomeRoute from "./routes/income.js";
+app.use("/inc", incomeRoute);
+
+// to get the port and the url from the .env file
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+const connection_url = process.env.CONNECTION_URL;
+
+// establish a connection and if success, start the app
+mongoose
+  .connect(connection_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () =>
+      console.log(`Server Running on Port: http://localhost:${port}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
--- server/routes/income.js
+++ server/routes/income.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("This works");
+  res.send("This works, from income");
 });
 
 export default router;
