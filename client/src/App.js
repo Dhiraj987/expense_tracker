d2bc01d5ce627dc24ef37129e1eccd407a263044
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-12-03 15:43:35 -0500

    initial front-end

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Switch, Rout, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="App">Hello, World!</div>;
+}
+
+export default App;
